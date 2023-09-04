47145fa10781b78cb265d4ab8c83d2abe251a5c1
paolocamagni <paolocamagni@yahoo.it>
2023-09-04 19:20:39 +0200

    "aggiunta classe Mucca e ProvaMucca"

---
+++ Mucca.java
@@ -0,0 +1,53 @@
+/**
+ * La mucca e' un simpatico animale che produce il latte
+ * 
+ * @author Paolo 
+ * @version 2.1 del 11/10/2023
+ */
+public class Mucca 
+{
+    // instance variables - replace the example below with your own
+    private int eta;
+    private int produzione;
+    private String nome;
+
+    /**
+     * Costruttore della Mucca
+     */
+    public Mucca()
+    {
+        // initialise instance variables
+        eta = 0;
+    }
+
+    public Mucca(String nome)
+    {
+        // initialise instance variables
+        eta = 0;
+        nome = nome;
+    }
+
+    /**
+     * metodo per effettare la mungitura
+     * 
+     * @param  litri_latte : litri di latte richiesti
+     * @return produzione  : litri di latte effettivamente munti
+     */
+    public int mungi(int litri_latte)
+    {
+        // put your code here
+        return produzione;
+    }
+    /**
+     * metodo per alimentare la mucca
+     * 
+     * @param  kg_fieno : quantita' di fieno dato in pasto
+     * @return ok       : conferma 
+     */
+    public boolean mangia (int kg_fieno)
+    {
+        boolean ok;
+        ok=true;
+        return ok;
+    }
+}
---
+++ ProvaMucca.java
@@ -0,0 +1,14 @@
+
+/**
+ * Aggiungi qui una descrizione della classe ProvaMucca
+ * 
+ * @author (il tuo nome) 
+ * @version (un numero di versione o una data)
+ */
+public class ProvaMucca
+{
+   public static void main(String args[])
+   {
+       Mucca Carolina = new Mucca("Carolina");   
+   }  
+}
